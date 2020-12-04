8118a9b308f7c4713e980dcba3c3b621cf04443d
paula andrea maldonado <paula.maldo97@gmail.com>
2020-12-04 16:25:02 -0500

    details css

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,8 +1,8 @@
-
-import { Card, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
+import { Card, CardContent, CardMedia, makeStyles, Typography, Button } from '@material-ui/core';
 import React from 'react';
 import Rating from '@material-ui/lab/Rating';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { addProductAction } from './../redux/actions';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -10,10 +10,12 @@ const useStyles = makeStyles((theme) => ({
         margin: '10px 50px',
         padding: 10,
         flexWrap: 'wrap',
+        width: '90%',
     },
     details: {
         display: 'flex',
         flexDirection: 'column',
+        maxWidth: "800px",
     },
     content: {
         flex: '1 0 auto',
@@ -25,24 +27,29 @@ const useStyles = makeStyles((theme) => ({
         width: 400,
         height: 400,
     },
-    container: {
-        margin: 5,
-        width: 200,
-        height: 'auto',
-        lineHeight: 115,
-        textAlign: "center",
-        border: "1px solid rgb(221, 221, 221)",
-    },
-    resizeFitCenter: {
-        maxWidth: "100%",
-        maxHeight: "100%",
-        verticalAlign: "middle",
+    content2: {
+        display: 'flex',
+        marginLeft: 'auto',
+        marginRight: 'auto',
 
-    }
+    },
+    button: {
+        margin: '10px 50px',
+        textTransform: "none",
+        borderRadius: 31,
+        background: "#772CE8",
+        width: 166,
+        height: 28,
+        "&:hover": {
+            background: "#772CE8",
+            boxShadow: "none"
+        },
+    },
 }));
 
-export default function Details(props) {
+export default function Details() {
     const classes = useStyles();
+    const dispatch = useDispatch();
     const item = useSelector(state => state.item);
 
     function formatCurrency(locales, currency, fractionDigits, number) {
@@ -85,18 +92,16 @@ export default function Details(props) {
                         </Typography>
                         <Typography style={{ margin: 20, color: "#5C5E64", display: 'inline' }} variant="h4" >
                             {(Math.floor((100 - (item.item.price * 100) / (item.item.price + item.item.price * .1)))) + "% OFF"}
-
                         </Typography>
                         {
                             item.item.seller.logo.includes("http") &&
-                            <div className={classes.container}>
+                            <div class='container'>
                                 <a href='#'>
-                                    <img className={classes.resize_fit_center}
+                                    <img class='resize_fit_center'
                                         src={item.item.seller.logo} />
                                 </a>
                             </div>
                         }
-
                         <Typography variant="h5" style={{ marginTop: 20, color: "#5C5E64" }}>
                             {item.item.seller.name}
                         </Typography>
@@ -104,7 +109,17 @@ export default function Details(props) {
                             {item.item.city.name}
                         </Typography>
                     </CardContent>
-
+                    <div className={classes.content2}>
+                        <Button className={classes.button} variant="contained" color="primary"
+                            onClick={() => {
+                                dispatch(addProductAction(item.item))
+                            }}>
+                            Agregar al carrito
+                        </Button>
+                        <Button className={classes.button} variant="contained" color="primary" >
+                            Comprar
+                        </Button>
+                    </div>
                 </div>
             </Card>
             <Card className={classes.root}>
